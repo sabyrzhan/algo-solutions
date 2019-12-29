d8bdf41919552bdf907eb2b1b147f341103df781
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2019-12-29 02:06:38 +0200

    Algoexpert: add easy solution from 9 problem

--- src/main/java/kz/sabyrzhan/algo/aloexpert/easy/T09_FindThreeLargestNumbers.java
+++ src/main/java/kz/sabyrzhan/algo/aloexpert/easy/T09_FindThreeLargestNumbers.java
@@ -4,17 +4,42 @@ import java.util.Arrays;
 
 public class T09_FindThreeLargestNumbers {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(Program.findThreeLargestNumbers(new int[] {10, 5, 9, 10, 12})));
+        System.out.println(Arrays.toString(Program.findThreeLargestNumbers(new int[] {141, 1, 17, -7, -17, -27, 18, 541, 8, 7, 7})));
     }
 
     static class Program {
         public static int[] findThreeLargestNumbers(int[] array) {
-            Arrays.sort(array);
-            if(array.length <= 3) {
+            if(array.length == 0 || array.length == 1) {
                 return array;
             }
 
-            return new int[] {array[array.length - 3], array[array.length - 2], array[array.length - 1]};
+            if(array.length == 2) {
+                return array[0] < array[1] ? new int[] {array[1], array[0]} : array;
+            }
+
+            int[] result = new int[3];
+            result[0] = Integer.MIN_VALUE;
+            result[1] = Integer.MIN_VALUE;
+            result[2] = Integer.MIN_VALUE;
+
+            for(int i : array) {
+                shift(result, i);
+            }
+
+            return result;
+        }
+
+        private static void shift(int[] result, int value) {
+            if(value >= result[2]) {
+                result[0] = result[1];
+                result[1] = result[2];
+                result[2] = value;
+            } else if(value >= result[1]) {
+                result[0] = result[1];
+                result[1] = value;
+            } else if(value >= result[0]) {
+                result[0] = value;
+            }
         }
     }
 }
