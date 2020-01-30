9b8da806bd38205c955148cc4e9d7f3c8e7b6601
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2020-01-30 22:36:27 +0200

    Algoexpert: add easy solution from 11 problem: InsertionSort

---
+++ src/main/java/kz/sabyrzhan/algo/aloexpert/easy/T11_InsertionSort.java
@@ -0,0 +1,30 @@
+package kz.sabyrzhan.algo.aloexpert.easy;
+
+import java.util.Arrays;
+
+public class T11_InsertionSort {
+    public static void main(String[] args) {
+        int[] ints = new int[] {1,2,3,4,5,6};
+        int[] result = Program.insertionSort(ints);
+        System.out.println(Arrays.toString(result));
+    }
+
+    static class Program {
+        public static int[] insertionSort(int[] array) {
+            int tmp;
+            for(int i = 1; i < array.length; i++) {
+                for(int j = i; j > 0; j--) {
+                    if(array[j - 1] > array[j]) {
+                        System.out.print("Before: " + Arrays.toString(array));
+                        tmp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = tmp;
+                        System.out.println(" After: " + Arrays.toString(array) + ", Swap: " + array[j - 1] + " : " + array[j]);
+                    }
+                }
+            }
+
+            return array;
+        }
+    }
+}
