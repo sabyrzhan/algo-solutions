a92e20ae884091abfa559fd1c4d14b18ae73c833
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2021-07-31 22:29:44 +0600

    Update MoveZeroes

--- src/main/java/kz/sabyrzhan/algo/leetcode/problems/p283/MoveZeroes.java
+++ src/main/java/kz/sabyrzhan/algo/leetcode/problems/p283/MoveZeroes.java
@@ -5,14 +5,12 @@ public class MoveZeroes {
         if (nums == null || nums.length == 0) {
             return;
         }
-
         int i = 0;
         for(int n : nums) {
             if (n != 0) {
                 nums[i++] = n;
             }
         }
-
         while(i < nums.length) {
             nums[i++] = 0;
         }
